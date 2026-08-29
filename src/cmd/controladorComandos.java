12ea28851f71de722ce4586f90d0ee46ebac42fa
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-08-29 10:34:52 -0600

    Arreglar y conectar con gestor

--- src/cmd/controladorComandos.java
+++ src/cmd/controladorComandos.java
@@ -18,7 +18,7 @@ public class controladorComandos {
         this.consola = consola;
         this.gestor = gestor;
         modoEscritura = false;
-        cmd2 = new ComandoCMD2(consola.getCarpetaRaiz());
+        cmd2 = new ComandoCMD2(gestor.getCarpetaActual());
     }
     
     public void ejecutarComando(String entrada){
@@ -100,11 +100,11 @@ public class controladorComandos {
                 if(partes.length < 2){
                     consola.imprimir("Uso: Info <nombre>");
                 } else {
-                    consola.imprimir(cmd2.info(partes[1]));
+                    consola.imprimir(gestor.obtenerInformacion(partes[1]));
                 }
                 break;
             case "Tree":
-                consola.imprimir(cmd2.tree());
+                consola.imprimir(gestor.generarArbol());
                 break;
             case "Cls":
                 consola.limpiar();
@@ -125,17 +125,7 @@ public class controladorComandos {
             consola.imprimir("Uso: Mkdir <nombre>");
             return;
         }
-        File carpeta = new File(consola.getCarpetaActual(), partes[1]);
-        if(carpeta.exists()){
-            consola.imprimir("Ya existe un archivo o carpeta con ese nombre.");
-            return;
-        }
-        if(carpeta.mkdir()){
-            consola.imprimir("Carpeta creada correctamente.");
-        }
-        else{
-            consola.imprimir("No se pudo crear la carpeta.");
-        }
+        consola.imprimir(gestor.crearDirectorio(partes[1]));
     }
     
     private void mFile(String[] partes){
@@ -143,18 +133,7 @@ public class controladorComandos {
             consola.imprimir("Uso: Mfile <nombre.ext>");
             return;
         }
-        File archivo = new File(consola.getCarpetaActual(), partes[1]);
-        if(archivo.exists()){
-            consola.imprimir("Ya existe un archivo o carpeta con ese nombre.");
-            return;
-        }
-        try{
-            FileWriter fw = new FileWriter(archivo);
-            fw.close();
-            consola.imprimir("Archivo creado correctamente");
-        }catch(IOException e){
-            consola.imprimir("Error creando el archivo: " + e.getMessage());
-        }
+        consola.imprimir(gestor.crearArchivo(partes[1]));
     }
     
     private void rm(String[] partes){
@@ -162,17 +141,7 @@ public class controladorComandos {
             consola.imprimir("Uso: Rm <nombre>");
             return;
         }
-        File archivo = new File(consola.getCarpetaActual(), partes[1]);
-        if(!archivo.exists()){
-            consola.imprimir("El archivo o carpeta no existe.");
-            return;
-        }
-        if(gestor.eliminarRecursivo(archivo)){
-            consola.imprimir("Eliminado correctamente.");
-        }
-        else{
-            consola.imprimir("No se pudo eliminar.");
-        }
+        consola.imprimir(gestor.eliminar(partes[1]));
     }
     
     private void cd(String[] partes){
@@ -180,53 +149,20 @@ public class controladorComandos {
             consola.imprimir("Uso: Cd <nombre carpete>");
             return;
         }
-        File destino = new File(consola.getCarpetaActual(), partes[1]);
-        if(!destino.exists()){
-            consola.imprimir("La carpeta indicada no existe.");
-            return;
-        }
-        if(!destino.isDirectory()){
-            consola.imprimir("El elemento indicado no es una carpeta");
-            return;
-        }
-        try{
-            if(!archivos.estaDentroRaiz(destino)){
-                consola.imprimir("No puede salir de la carpeta raíz.");
-                return;
-            }
-        }catch(IOException e){
-            consola.imprimir("Error accediendo a la ruta.");
-            return;
-        }
-        consola.setCarpetaActual(destino);
+        String resultado = gestor.cambiarDirectorio(partes[1]);
+        consola.imprimir(resultado);
+        consola.setCarpetaActual(gestor.getCarpetaActual());
+        cmd2.setCarpetaActual(gestor.getCarpetaActual());
     }
     
     private void regresar(){
-        File actual = consola.getCarpetaActual();
-        if(actual.equals(consola.getCarpetaRaiz())){
-            consola.imprimir("Ya se encuentra en la carpeta raíz.");
-            return;
-        }
-        File anterior = actual.getParentFile();
-        if(anterior != null){
-            consola.setCarpetaActual(anterior);
-        }
+        consola.imprimir(gestor.subirDirectorio());
+        consola.setCarpetaActual(gestor.getCarpetaActual());
+        cmd2.setCarpetaActual(gestor.getCarpetaActual());
     }
     
     private void dir(){
-        File[] contenido = consola.getCarpetaActual().listFiles();
-        if(contenido == null || contenido.length == 0){
-            consola.imprimir("La carpeta esta vacía.");
-            return;
-        }
-        for(File archivo : contenido){
-            if(archivo.isDirectory()){
-                consola.imprimir("<DIR>     " + archivo.getName());
-            }
-            else{
-                consola.imprimir("          " + archivo.getName());
-            }
-        }
+         consola.imprimir(gestor.listarDirectorio());
     }
     
     private void date(){
@@ -290,42 +226,7 @@ public class controladorComandos {
             consola.imprimir("Uso: Rd <archivo.ext>");
             return;
         }
-        String nombreArchivo = partes[1];
-        File archivo = new File(
-                consola.getCarpetaActual(),
-                nombreArchivo
-        );
-        if(!archivo.exists()){
-            consola.imprimir("El archivo no existe.");
-            return;
-        }
-        if(!archivo.isFile()){
-            consola.imprimir(
-                    "El nombre indicado no corresponde a un archivo."
-            );
-            return;
-        }
-        try{
-            FileReader lector = new FileReader(archivo);
-            String contenido = "";
-            int caracter = lector.read();
-            while(caracter != -1){
-                contenido += (char) caracter;
-                caracter = lector.read();
-            }
-            lector.close();
-            if(contenido.isEmpty()){
-                consola.imprimir(
-                        "El archivo esta vacío."
-                );
-            }else{
-                consola.imprimir(contenido);
-            }
-        }catch(IOException e){
-            consola.imprimir(
-                    "Error al leer el archivo."
-            );
-        } 
+        consola.imprimir(gestor.leerArchivo(partes[1]));
     }
        
 }
